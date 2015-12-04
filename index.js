a7c821b89cea8a13f5cbb31679098c6f6f7fb874
_FR_Starfox64 <louisdijon21@yahoo.fr>
2015-12-04 21:19:15 +0100

    Added google command

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var Supinbot = require('./libs/supinbot');
+var querystring = require('querystring');
 var fs = require('fs');
 
 var BOT_CONFIG = JSON.parse(fs.readFileSync('./bot-config.json'));
@@ -15,4 +16,9 @@ SupinBot.CommandManager.addCommand('add', function(bot, user, channel, args, arg
 
 SupinBot.CommandManager.addCommand('time', function(bot, user, channel, args, argsStr) {
 	bot.postMessage(channel.id, String(new Date()), PARAMS);
+});
+
+SupinBot.CommandManager.addCommand('google', function(bot, user, channel, args, argsStr) {
+	var url = 'http://lmgtfy.com/?' + querystring.stringify({q: argsStr});
+	bot.postMessage(channel.id, url + ':expressionless:', PARAMS);
 });
